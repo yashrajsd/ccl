2ee0a855f157423ba8e6a98c4faf0e02927fc85f
Yashraj Deshmukh <107096154+yashrajsd@users.noreply.github.com>
2024-04-30 14:31:39 +0530

    Update dbconfig.js

--- dbconfig/dbconfig.js
+++ dbconfig/dbconfig.js
@@ -2,8 +2,8 @@ const Pool = require('mysql');
 
 const pool = new Pool({
     user: 'admin',
-    host: 'db.claky8uci7yd.us-east-1.rds.amazonaws.com',
-    database: 'db',
+    host: 'ccldb.cv4oagoqevyh.us-east-1.rds.amazonaws.com',
+    database: 'ccldb',
     password: 'YDArchu77',
     port: 3306
 });
